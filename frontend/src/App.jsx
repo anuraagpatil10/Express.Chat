d7abc8b3549076f6c00a30832b6bc0befb975f4a
anuraagpatil10 <anuraagpatil10@gmail.com>
2025-06-06 02:37:02 +0530

    getting started with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Routes, Route } from 'react-router-dom';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 
-export default App
+const App = () => {
+  
+  return (<div>
+
+    <Navbar/>
+
+    <Routes>
+
+    <Route path ="/" element ={<HomePage/>} />
+    {/* <Route path ="/about" element ={<About/>} /> */}
+    <Route path ="/signup" element ={<SignUpPage/>} />
+    <Route path ="/login" element ={<LoginPage/>} />
+    <Route path ="/settings" element ={<SettingsPage/>} /> 
+    <Route path ="/profile" element ={<ProfilePage/>} />
+
+    </Routes>
+
+  </div>);
+  
+};
+export default App;

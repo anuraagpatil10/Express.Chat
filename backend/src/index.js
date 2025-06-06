389e6f81371b0a6a0e1a2753054239ac034a5c85
anuraagpatil10 <anuraagpatil10@gmail.com>
2025-06-06 23:48:04 +0530

    created routes for all pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ import dotenv from 'dotenv';
 
 import cookieParser from 'cookie-parser';
 
+import cors from "cors";
+
 import {connectDB} from './lib/db.js';
 
 dotenv.config();
@@ -18,6 +20,11 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar.jsx';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import HomePage from './pages/HomePage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
@@ -8,20 +8,37 @@ import LoginPage from './pages/LoginPage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 
+import { useAuthStore } from './store/useAuthStore.js';
+
+import { useEffect } from 'react';
+
+import { Loader } from 'lucide-react';
+
 const App = () => {
   
+  const {authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {checkAuth()}, []);
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) return (
+    <div className = "flex items-center justify-center h-screen">
+      <Loader className = "size-10 animate-spin" />
+    </div>)
+
   return (<div>
 
     <Navbar/>
 
     <Routes>
 
-    <Route path ="/" element ={<HomePage/>} />
+    <Route path ="/" element = { authUser ? <HomePage/> : <Navigate to = "/login" /> } />
     {/* <Route path ="/about" element ={<About/>} /> */}
-    <Route path ="/signup" element ={<SignUpPage/>} />
-    <Route path ="/login" element ={<LoginPage/>} />
-    <Route path ="/settings" element ={<SettingsPage/>} /> 
-    <Route path ="/profile" element ={<ProfilePage/>} />
+    <Route path ="/signup" element = { !authUser ? <SignUpPage/> : <Navigate to = "/" /> } />
+    <Route path ="/login" element = { !authUser ? <LoginPage/> : <Navigate to = "/" /> } />
+    <Route path ="/settings" element = { authUser ? <SettingsPage/> : <Navigate to = "/login" /> } /> 
+    <Route path ="/profile" element = { authUser ? <ProfilePage/> : <Navigate to = "/login" /> } />
 
     </Routes>
 
